f59d1c860cc183c6d2286627630aac47f76a8542
AndrewHelix <andreyshulga95@gmail.com>
2021-09-15 20:34:48 +0300

    feat: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  const newArr = [];
+
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      if (i != arr.length - 1) {
+        i++;
+      }
+      if (arr[i + 1] === "--discard-prev" || 
+          arr[i + 1] === "--double-prev") {
+          i++;
+      }
+    } else if (arr[i] === "--discard-prev") {
+      if (i > 0) {
+        newArr.pop();
+      }
+    } else if (arr[i] === "--double-next") {
+      if (i != arr.length - 1) {
+        newArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === "--double-prev") {
+      if (i > 0) {
+        newArr.push(arr[i - 1]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
 }
+
